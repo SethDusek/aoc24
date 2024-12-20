498de3c390bbff895a1f70536f870b7b8febf3bf
Kamal Ahmad <kamalahmad22@pm.me>
2024-12-20 11:28:30 +0500

    slightly less messy & optimized day20 part 1

--- day20/src/main.rs
+++ day20/src/main.rs
@@ -1,5 +1,7 @@
 use std::collections::{HashMap, HashSet, VecDeque};
 
+use arrayvec::ArrayVec;
+
 const TEST: &str = include_str!("../test.txt");
 const INPUT: &str = include_str!("../input.txt");
 
@@ -18,8 +20,19 @@ fn parse_grid(input: &str) -> HashMap<(i64, i64), char> {
 #[derive(PartialOrd, PartialEq, Clone, Hash, Eq, Debug)]
 struct Node {
     pos: (i64, i64),
-    cheat_positions: Vec<(i64, i64)>,
+    cheat_positions: ArrayVec<(i64, i64), 20>,
     steps: usize,
+}
+
+fn unique_pos(positions: &[(i64, i64)]) -> ArrayVec<(i64, i64), 2> {
+    let mut v = ArrayVec::new();
+    if let Some(start) = positions.first() {
+        v.push(*start);
+    }
+    if let Some(end) = positions.last() {
+        v.push(*end);
+    }
+    v
 }
 fn bfs(
     grid: &HashMap<(i64, i64), char>,
@@ -33,17 +46,15 @@ fn bfs(
     let mut visited = HashSet::new();
     queue.push_back(Node {
         pos: start,
-        cheat_positions: vec![],
+        cheat_positions: ArrayVec::new(),
         steps: 0,
     });
-    let mut atleast_count = 0;
     let mut unique = HashSet::new();
     while let Some(cur) = queue.pop_front() {
         if cur.pos == end {
             if let Some(without) = without_cheats {
-                if cur.steps.abs_diff(without) >= atleast {
+                if dbg!(cur.steps.abs_diff(without)) >= atleast {
                     unique.insert(cur.cheat_positions.clone());
-                    atleast_count += 1;
                 } else {
                     break;
                 }
@@ -53,12 +64,13 @@ fn bfs(
             println!("Goal at {} steps", cur.steps);
             continue;
         }
-        if visited.contains(&(cur.pos, cur.cheat_positions.clone()))
-            || cur.cheat_positions.len() == 2 && grid.get(&cur.pos) == Some(&'#')
+        if visited.contains(&(cur.pos, unique_pos(&cur.cheat_positions)))
+            || cur.cheat_positions.len() == cur.cheat_positions.capacity()
+                && grid.get(&cur.pos) == Some(&'#')
         {
             continue;
         }
-        visited.insert((cur.pos, cur.cheat_positions.clone()));
+        visited.insert((cur.pos, unique_pos(&cur.cheat_positions)));
         let adj = [(0, -1), (0, 1), (1, 0), (-1, 0)];
         for adj in adj {
             let next_pos = (cur.pos.0 + adj.0, cur.pos.1 + adj.1);
@@ -67,25 +79,27 @@ fn bfs(
                 continue;
             }
             if grid.get(&next_pos) == Some(&'#') {
-                if !cheats_allowed || cur.cheat_positions.len() >= 2 {
+                if !cheats_allowed || cur.cheat_positions.len() >= cur.cheat_positions.capacity() {
                     continue;
                 }
                 needs_cheat = true;
             }
 
-            let cheat_positions: Vec<_> = if cheats_allowed
-                && (needs_cheat || cur.cheat_positions.len() > 0 && cur.cheat_positions.len() < 2)
+            let cheat_positions: ArrayVec<_, 20> = if cheats_allowed
+                && (needs_cheat
+                    || cur.cheat_positions.len() > 0
+                        && cur.cheat_positions.len() < cur.cheat_positions.capacity())
             {
                 cur.cheat_positions
                     .iter()
                     .copied()
                     .chain(std::iter::once(cur.pos))
-                    .take(2)
+                    .take(cur.cheat_positions.capacity())
                     .collect()
             } else {
                 cur.cheat_positions.clone()
             };
-            if visited.contains(&(next_pos, cheat_positions.clone())) {
+            if visited.contains(&(next_pos, unique_pos(&cheat_positions))) {
                 continue;
             }
             queue.push_back(Node {
@@ -95,9 +109,7 @@ fn bfs(
             });
         }
     }
-    println!("atleast: {atleast_count}");
     println!("atleast: {}", unique.len());
-    println!("{unique:?}");
     None
 }
 
@@ -106,7 +118,6 @@ fn part1(input: &str) {
     let start = *parsed.iter().find(|(_, v)| **v == 'S').unwrap().0;
     let end = *parsed.iter().find(|(_, v)| **v == 'E').unwrap().0;
     let without_cheats = bfs(&parsed, start, end, false, None, 0).unwrap();
-    dbg!(without_cheats);
     bfs(&parsed, start, end, true, Some(without_cheats), 100);
 }
 fn main() {
